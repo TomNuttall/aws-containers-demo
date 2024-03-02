1c3a4eb0dc7335c563e00e182427eaf507941b62
Tom <tsnuttall@gmail.com>
2024-03-02 16:17:39 +0000

    app: Use temp folder

--- app/src/app.py
+++ app/src/app.py
@@ -1,15 +1,26 @@
-import boto3
-import os
 import argparse
+import boto3
 from dotenv import load_dotenv
 from pathlib import Path
+import shutil
+import os
 from zipfile import ZipFile
+
+
+def setupEnv():
+  """ ."""
+
+  parser = argparse.ArgumentParser()
+  parser.add_argument('-envPath')
+  args = parser.parse_args()
+
+  if args.envPath:
+    load_dotenv(dotenv_path=Path(args.envPath))
 
 
 def get_object(bucket_name, object_name):
   """ ."""
 	
-  print(bucket_name, object_name)
   if not bucket_name or not object_name:
     return
   
@@ -19,18 +30,25 @@ def get_object(bucket_name, object_name):
 
   with ZipFile('temp.zip', 'r') as archive:  
     archive.extractall()
+  os.remove('temp.zip')
 
   print(os.listdir())
 
-  #client.upload_file(file_name, os.envrion("REPORT_BUCKET"), object_name)
-
 	
 if __name__ == "__main__":
-  parser = argparse.ArgumentParser()
-  parser.add_argument('-envPath')
-  args = parser.parse_args()
-
-  if args.envPath:
-    load_dotenv(dotenv_path=Path(args.envPath))
+  setupEnv()
   
+  # Create temp folder
+  temp_path = os.path.join(os.getcwd(), "temp")
+  if not os.path.isdir(temp_path):
+    os.mkdir(temp_path)      
+  os.chdir(temp_path)
+
+  # 
   get_object(os.environ.get("INGRESS_BUCKET"), os.environ.get("S3_KEY"))
+
+  # Cleanup
+  shutil.rmtree(temp_path)
+
+
+  #client.upload_file(file_name, os.envrion("REPORT_BUCKET"), object_name)
